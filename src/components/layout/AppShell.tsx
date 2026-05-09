af6f7a97d2efe74aa8c884bafa02458e1567e9da
Saurabh <Saurabh.jain@heady.io>
2026-05-09 22:39:16 +0530

    refactor: remove TitleBar component from AppShell layout

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import React from "react";
-import TitleBar from "@/components/ui/TitleBar";
 import { AppProviders } from "@/components/providers/AppProviders";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <AppProviders>
-      <TitleBar />
       {children}
     </AppProviders>
   );
