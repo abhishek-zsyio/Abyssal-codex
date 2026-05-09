305b1193956cb8e01c4a7e78f70e864cc417911d
Saurabh <Saurabh.jain@heady.io>
2026-05-09 22:36:24 +0530

    refactor: remove Electron-related configuration and unused Neutralinojs files

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
-const isTauri = process.env.IS_TAURI === "true";
-
 const nextConfig: NextConfig = {
-  // output: "export",
-  // trailingSlash: true,
   images: {
     unoptimized: true,
   },
